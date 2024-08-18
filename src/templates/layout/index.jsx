599bba87471461935ebc0d97bd07a7dc53f7d1f1
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-18 00:20:23 -0300

    style: chore: remover comentários e imports não utilizados

--- src/templates/layout/index.jsx
+++ src/templates/layout/index.jsx
@@ -1,11 +1,11 @@
 // Importa hooks e componentes do React e bibliotecas externas.
 import { FaArrowLeft } from "react-icons/fa";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import "../../index.css";
-import { AppContainer, Footer, MainContent } from "./style";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Button } from "../../assets/style-global";
 import { Nav } from "../../components/nav-bar";
+import "../../index.css";
+import { AppContainer, Footer, MainContent } from "./style";
 
 // Define o componente principal do aplicativo.
 export function Layout() {
@@ -19,7 +19,7 @@ export function Layout() {
   // Renderiza o componente principal.
   return (
     <AppContainer>
-      <Nav handleReturn={handleReturn} />
+      <Nav />
       <MainContent>
         <Outlet />
         {location.pathname != "/" && (
@@ -34,4 +34,4 @@ export function Layout() {
   );
 }
 
-// Exporta o componente App para ser utilizado em outras partes da aplicação.
+
