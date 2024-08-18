dde74a5a88594f2635a9c7700b69566023bf1cc8
SamilisBrito <samilisbrito.ufra@gmail.com>
2024-08-18 00:12:47 -0300

    fix: logout na nav

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -9,6 +9,7 @@ import {
   FaTasks,
 } from "react-icons/fa";
 import { ButtonLogout, NavBar, NavBarToggle, StyledLink } from "./style";
+import { useNavigate } from "react-router-dom";
 
 const links = [
   {
@@ -44,21 +45,29 @@ const links = [
   },
 ];
 
-export const Nav = ({ handleReturn }) => {
+export const Nav = () => {
   // Cria estado visibilidade da barra de navegação
   const [isNavBarOpen, setIsNavBarOpen] = useState(false);
-  
+  // Obtém a função de navegação
+  const navigate = useNavigate();
   // Alterna a visibilidade da barra de navegação.
   const toggleNavBar = () => {
     setIsNavBarOpen(!isNavBarOpen);
   };
+
+  // Função para handle do logout
+  const handleLogout = () => {
+    localStorage.removeItem("token"); // Remove o token do localStorage
+    navigate("/login"); // Redireciona para a página de login
+  };
+
   return (
     <>
       <NavBarToggle onClick={toggleNavBar}>
         <FaBars size={24} color="#2C3E50" />
       </NavBarToggle>
 
-      <NavBar isOpen={isNavBarOpen}>
+      <NavBar $isOpen={isNavBarOpen}>
         {links.map(({ id, icon, text, route }) => (
           <StyledLink key={id} to={route}>
             {icon}
@@ -66,7 +75,7 @@ export const Nav = ({ handleReturn }) => {
           </StyledLink>
         ))}
 
-        <ButtonLogout onClick={() => handleReturn("/login")} to={"login"}>
+        <ButtonLogout onClick={() => handleLogout()} to={"login"}>
           Logout
         </ButtonLogout>
       </NavBar>
